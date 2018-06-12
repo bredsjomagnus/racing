d8ca905ca99c03f8cb3391f6a275c2c16e8a2a9d
Magnus Andersson <magnusandersson076@gmail.com>
2018-06-12 11:10:12 +0000

    Editing welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,14 +83,14 @@
                 </div>
 				<center>
 					<i>Välkomstvy. Information skall läggas in. Sidan kommer vara tillänglig för alla besöker.</i>
-
+<!--
                <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                </div> -->
             </div>
         </div>
     </body>

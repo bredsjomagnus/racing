a7e8f85d60dd331d9baf056c46377cc1a5f4f260
Magnus Andersson <magnusandersson076@gmail.com>
2018-06-13 14:23:59 +0200

    Added short csv instructions

--- resources/views/data/addtrackdata.blade.php
+++ resources/views/data/addtrackdata.blade.php
@@ -5,29 +5,20 @@
 @section('content')
 	<h1>LÄGG TILL TRACKDATA VIA CSV-fil</h1>
 	<div class="row">
-		<div class="col-md-4">
-			<h2>CSV</h2>
+		<div class="col-md-12">
+			<h3>CSV</h3>
 			<p>För att lägga till via csv-fil måste filen hålla en viss struktur för att servern skall kunna bearbeta informationen och kunna lägga ner den i databasen.</p>
+			<h4>HEADER</h4>
+			<!-- <img src="{{ asset('img/csvheader.png') }}" alt="csv header"> -->
 			<p>Översta raden måste innehålla rubrikerna för de olika kolumnerna. Det är inte viktigt exakt vad det står (svenska eller engelska). Men följande kolumner skall finnas och i denna ordningen:</p>
-			<ul>
-				<li>#</li>
-				<li>Name</li>
-				<li>Lap Time</li>
-				<li>Speed</li>
-				<li>Elapsed Time</li>
-				<li>Passing Time</li>
-				<li>Hits</li>
-				<li>Strength</li>
-				<li>Noice</li>
-				<li>Photocell Time</li>
-				<li>Transponder</li>
-				<li>Background TX</li>
-				<li>Background Passing Time</li>
-			</ul>
-			<h3>Semikolon som separator</h3>
+		</div>
+		<div class='col-md-12'>
+			<p><strong>Rubrikrad:</strong> <i>#, name, lap time, speed, elapsed time, passed time, hits, strength, noice, photocell time, transponder, backup_tx, backup_passing_time</i></p>
+			<h4>Semikolon som separator</h4>
+			<!-- <img src="{{ asset('img/semikoloncsv.png') }}" alt="csv header"> -->
 			<p>När man skapar en csv-fil kan det ibland väljas hur de olika värdena skall seprareras. Det är viktigt att man här väljer semikolon (;) som separator för att servern skall kunna behandla och spara ner filen i databasen.</p>
 		</div>
-		<div class="col-md-8">
+		<div class="col-md-12">
 			<table width="600px">
 				<form action="{{ route('addtrackdataconfirm') }}" method="post" enctype="multipart/form-data">
 					@csrf
@@ -42,6 +33,10 @@
 					</tr>
 				</form>
 			</table>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
 			<br>
 			@if(count($trackinputs) > 0)
 				<h2>Vill du lägga till följande data?</h2>
@@ -64,6 +59,9 @@
 			@endif
 		</div>
 	</div>
+
+
+
 
 
 
